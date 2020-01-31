6f899748d8bf7068034f28120f7cad1ac456389d
Bernd Bohmeier <berndbohmeier@googlemail.com>
2020-01-31 14:46:00 +0100

    Fix newsletter box
    Fix the wrong private policy link and remove unnecessary link to GDPR

--- src/js/newsletter/components/ConditionsCheckbox.js
+++ src/js/newsletter/components/ConditionsCheckbox.js
@@ -11,16 +11,8 @@ export default function ConditionsCheckbox(props) {
       <label className="has-text-justified is-size-7">
         By checking this box you acknowledge that your information will be
         transferred to Mailchimp for processing and agree to our&nbsp;
-        <a href="/terms-conditions" target="_blank">
+        <a href="/legal-imprint-privacy-policy.html" target="_blank">
           pricacy policy
-        </a>
-        &nbsp;and the&nbsp;
-        <a
-          href="https://gdpr-info.eu/"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          GDPR
         </a>
         .
       </label>
